10f76bd29be8883d367844d5791a2eac17161456
anupnepal57 <46957675+anupnepal57@users.noreply.github.com>
2021-07-22 18:04:55 +0545

    Update JavaScriptExec.java

--- javaScriptUtil/JavaScriptExec.java
+++ javaScriptUtil/JavaScriptExec.java
@@ -3,13 +3,16 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 public class JavaScriptExec {
-	/*public static void scrollIntoView(WebElement element, WebDriver driver) {
+	//to scroll upto required area//
+	
+	public static void scrollIntoView(WebElement element, WebDriver driver) {
 	JavascriptExecutor js=((JavascriptExecutor)driver);
 	js.executeScript("arguments[0].scrollIntoView(true);", element);
-	}*/
+	}
 
 
-
+	//to scroll down of the p[age//
+	
 	public static void scrollPageDown(WebDriver driver) {
 	JavascriptExecutor js=((JavascriptExecutor) driver);
 	js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
